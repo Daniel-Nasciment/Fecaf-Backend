578707ffb8ffa4fd63233a11a08e2f6f28297eac
Daniel-Nasciment <daniel.fecaf@outlook.com>
2021-03-23 14:30:46 -0300

    Configurações do index.js para responder a requisição

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('Teste')
+const express = require('express');
+const path = require('path');
+const config = require('config');
+
+const port = process.env.PORT || config.get('server.port');
+
+const pedidos = [
+    {cliente: 'Daniel', sabor: 'Pizza de Strogonoff', tamanho: 'Grande', quantidade: '1'},
+    {cliente: 'Daniel', sabor: 'Pizza de Strogonoff', tamanho: 'Grande', quantidade: '1'}
+  ];
+
+const app = express();
+
+app.set('port', port);
+
+app.route('/pedido').get(
+    (req, res) => {
+        res.status(200).json(pedidos)
+    }
+)
+
+app.listen(port, () => {
+    console.log("Servidor iniciado na porta " + port)
+})
